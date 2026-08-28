5a6d82cc1645e22cffc0601ba2e463764a189a9e
Irwansyah <vlorleon22@gmail.com>
2026-08-28 13:59:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
-  alert('woy bro! Kangen ya?');
+  alert('Produk sudah habis dan belum tersedia');
 });
